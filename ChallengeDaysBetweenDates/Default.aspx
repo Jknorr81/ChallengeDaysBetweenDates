a8e57a0ad437c722e4ba31f931f04a2451c2f71c
James Knorr <jknorr@coachmeapps.com>
2018-01-26 15:02:04 -0800

    upload Challenge

---
+++ ChallengeDaysBetweenDates/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeDaysBetweenDates.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>How many days have elapsed?</h1>
+        <br />
+        Select one date:<br />
+        <asp:Calendar ID="firstCalendar" runat="server">
+            <OtherMonthDayStyle Font-Strikeout="True" ForeColor="Red" Wrap="True" />
+        </asp:Calendar>
+        <br />
+        <br />
+        Select a second date:<br />
+        <asp:Calendar ID="secondCalendar" runat="server">
+            <OtherMonthDayStyle Font-Strikeout="True" ForeColor="Red" />
+        </asp:Calendar>
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeDaysBetweenDates/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeDaysBetweenDates
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /*int firstCal = (int)firstCalendar.SelectedDate.Day;
+            int secondCal = (int)secondCalendar.SelectedDate.Day;
+
+            if (firstCal > secondCal)
+            {
+                int result = firstCal - secondCal;
+                resultLabel.Text = result.ToString() + " days have elapsed";
+            }
+            else if (secondCal > firstCal)
+            {
+                int result = (secondCal - firstCal);
+                resultLabel.Text = result.ToString() + " days have elapsed";      
+            }
+            else
+            {
+                resultLabel.Text = "0 days have elapsed";
+            }
+            */
+
+            if (firstCalendar.SelectedDate > secondCalendar.SelectedDate)
+            {
+                resultLabel.Text = firstCalendar.SelectedDate.Subtract(secondCalendar.SelectedDate).TotalDays.ToString();
+            }
+            else
+            {
+                resultLabel.Text = secondCalendar.SelectedDate.Subtract(firstCalendar.SelectedDate).TotalDays.ToString();
+            }
+        }
+    }
+}
